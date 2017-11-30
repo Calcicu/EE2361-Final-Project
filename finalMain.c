5265c3fa4161b40c15eedc0c84c4fec3e7d7fc18
cjsvare <33737352+cjsvare@users.noreply.github.com>
2017-11-30 09:49:15 -0600

    Update finalMain.c

--- finalMain.c
+++ finalMain.c
@@ -2,14 +2,13 @@
  * File:   thom6223_lab5main.c
  * Author: Jake Thompson, Carol Svare, Mai Hashad, Rachel Chadwick
  *
- * Created on November 21, 2017, 11:45 AM
+ * Created on November 29, 2017, 4:00 PM
  */
 
 #include "xc.h"
 #include <p24Fxxxx.h>
 #include "lcdLib.h"
 #include <stdio.h>
-#include <math.H>
 
 // CW1: FLASH CONFIGURATION WORD 1 (see PI24 Family Reference Manual 24.1)
 #pragma config ICS = PGx1           // Comm Channel Select (Emulator EMUC1/EMUD1 pins areshared with PGC1/PGD1)
@@ -26,3 +25,43 @@
 #pragma config FCKSM = CSECME       // Clock Switching and Monitor (Clock switching is enabled,
                                         // Fail-Safe Clock Monitor is enabled)
 #pragma config FNOSC = FRCPLL       // Oscillator Select (Fast RC Oscillator with PLL module (FRCPLL))
+
+void setup(void);
+
+
+int main()
+{
+  
+  return 0;
+}
+
+void setup(void)
+{
+  CLKDIVbits.RCDIV = 0; //set Fcy to 16MHz
+  //AD1PCFG = 
+  //TRISA = 
+  //TRISB = 
+  
+  //inittialise i2c for LCD and LED
+  
+  /*initialize ADC for joystick
+    
+    _VCFG = 
+    _ADCS = 
+    _ASAM = 
+    _SSRC = 
+    _SAMC = 
+    _SMPI = 
+    
+    initialize interrupt:
+    _AD1IF = 0; // set interrupt flag to 0
+    _AD1IP =    //set interrupt priority
+    _AD1IE = 1; //enable interrupt
+    
+    
+    
+    
+  */
+  
+  
+}
